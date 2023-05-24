66c64ab070b33f9f957fee0b3ed10beec1077076
J-P Nurmi <jpnurmi@ubuntu.com>
2023-05-24 22:42:50 +0200

    Release 0.0.1

--- lib/odrs.dart
+++ lib/odrs.dart
@@ -1,3 +1,6 @@
+/// Open Desktop Ratings Service ([ORDS](https://odrs.gnome.org/)) client for Dart.
+library odrs;
+
 export 'src/client.dart';
 export 'src/exception.dart';
 export 'src/rating.dart';
